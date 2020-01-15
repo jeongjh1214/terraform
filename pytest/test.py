90c66a55c21c27dc1d8b4b14e1c7cf7e4efe5c0f
jeongjh <rubyeye1214@gmail.com>
2020-01-15 06:11:55 +0000

    20200115_backup

--- pytest/test.py
+++ pytest/test.py
@@ -10,15 +10,22 @@ command2 = os.popen("aws ec2 describe-instance-status --query InstanceStatuses[*
 a = json.load(command)
 b = json.load(command2)
 
+#for i in a:
+#    if (i['ResourceType']) == 'instance' and (i['Key']).lower() == ('deleteon').lower():
+#        bb = time.mktime(datetime.strptime(i['Value'], '%Y-%m-%d').timetuple())
+#        today = time.mktime(datetime.today().timetuple())
+#
+#        if bb-today < 0:
+#            instanceid = (i['ResourceId'])
+#
+#            for j in b:
+#                if j['InstanceId'] == instanceid and j['InstanceState']['Name'] == 'running':
+#                    os.system('aws ec2 stop-instances --instance-ids %s' %instanceid)
+#                    print (instanceid + " Stop")
+
 for i in a:
-    if (i['ResourceType']) == 'instance' and (i['Key']).lower() == ('deleteon').lower():
-        bb = time.mktime(datetime.strptime(i['Value'], '%Y-%m-%d').timetuple())
-        today = time.mktime(datetime.today().timetuple())
-
-        if bb-today < 0:
-            instanceid = (i['ResourceId'])
-
-            for j in b:
-                if j['InstanceId'] == instanceid and j['InstanceState']['Name'] == 'running':
-                    os.system('aws ec2 stop-instances --instance-ids %s' %instanceid)
-                    print (instanceid + " Stop")
+    if (i['ResourceType']) == 'instance':
+        print (i)
+    else:
+        print ("test")
+        print (i)
--- pytest/test1.py
+++ pytest/test1.py
@@ -1,34 +1,50 @@
 #!/bin/python3
 
-import json, os
+import json, os, sys
 from datetime import datetime
 import time
 
+
 command = os.popen("aws ec2 describe-instances --query Reservations[*]")
+command2 = os.popen("aws ec2 describe-tags --query Tags[*]")
 
 ec2infos = json.load(command)
+ec2tagsinfos = json.load(command2)
+
+#deleteon 있는 서버
+deloninstanceids = []
+
+# deleteon 없는 서버
+deloninstanceids2 = []
 
 for ec2info in ec2infos:
     ec2ins = ec2info['Instances']
     for ec2in in ec2ins:
-        for ec2tag in ec2in['Tags']:
-            if ec2tag['Key'].lower() == "DeleteOn".lower():
-                ec2deltime = time.mktime(datetime.strptime(ec2tag['Value'], '%Y-%m-%d').timetuple())
-                today = time.mktime(datetime.today().timetuple())
+        if 'Tags' not in ec2in.keys():
+            print ("no tags " + ec2in['InstanceId'])
+        else:
+            delcount = 0
+            for ec2tag in ec2in['Tags']:
+                if "DeleteOn".lower() in ec2tag['Key'].lower():
+                    delcount+=1
 
-                if ec2deltime - today < 0:
-                    instanceids = ec2in['InstanceId']
-                    if ec2in['State']['Name'] == 'running':
-                        print (ec2in)
+            if delcount == 0:
+                deloninstanceids2.append(ec2in['InstanceId'])
+            elif delcount == 1:
+                deloninstanceids.append(ec2in['InstanceId'])
+            elif delcount > 1:
+                print ("delete 설정이 여러개 되어있습니다")
 
-#    if (ec2info['ResourceType']) == 'instance' and (i['Key']).lower() == ('deleteon').lower():
-#        bb = time.mktime(datetime.strptime(i['Value'], '%Y-%m-%d').timetuple())
-#        today = time.mktime(datetime.today().timetuple())
-#
-#        if bb-today < 0:
-#            instanceid = (i['ResourceId'])
-#
-#            for j in b:
-#                if j['InstanceId'] == instanceid and j['InstanceState']['Name'] == 'running':
-#                    os.system('aws ec2 stop-instances --instance-ids %s' %instanceid)
-#                    print (instanceid + " Stop")
+for delinstance in deloninstanceids:
+    for ec2tags in ec2tagsinfos:
+        if (ec2tags['ResourceType']) == 'instance' and (ec2tags['ResourceId']) == delinstance and (ec2tags['Key']).lower() == ('deleteon').lower():
+            deltime = time.mktime(datetime.strptime(ec2tags['Value'], '%Y-%m-%d').timetuple())
+            today = time.mktime(datetime.today().timetuple())
+
+            if deltime - today < 0:
+                os.system('aws ec2 stop-instances --instance-ids %s' %delinstance)
+                print (delinstance + " Stop1")
+
+for delinstance1 in deloninstanceids2:
+    os.system('aws ec2 stop-instances --instance-ids %s' %delinstance1)
+    print (delinstance1 + " Stop2")
